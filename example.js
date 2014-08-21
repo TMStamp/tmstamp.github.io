36230b5bc1fc678a0d8549a1e16bff78f7c7279e
Thierry Mayrand <thierry.mayrand@gmail.com>
2014-08-21 00:29:33 -0400

    123

--- example.js
+++ example.js
@@ -1,6 +1,9 @@
 var myApp = angular.module('plunker', ['ui.bootstrap']);
 /*
 myApp.controller('dropDownController', ['$scope', function($scope) {
+   $scope.status = {
+      isopen: false
+   };
 }]);
 
 */
