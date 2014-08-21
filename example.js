fe35ff7db822c189d2580111fc5c2a169e91706b
Thierry Mayrand <thierry.mayrand@gmail.com>
2014-08-20 22:42:24 -0400

    123

--- example.js
+++ example.js
@@ -1,17 +1,7 @@
 angular.module('plunker', ['ui.bootstrap']);
 function DropdownCtrl($scope) {
-  $scope.items = [
-    'The first choice!',
-    'And another choice for you.',
-    'but wait! A third!'
-  ];
-
   $scope.status = {
     isopen: false
-  };
-
-  $scope.toggled = function(open) {
-    console.log('Dropdown is now: ', open);
   };
 
   $scope.toggleDropdown = function($event) {
