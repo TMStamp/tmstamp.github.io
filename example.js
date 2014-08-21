9000164c29f0a6567bdbcf7f1e7737b2a34e3ea1
Thierry Mayrand <thierry.mayrand@gmail.com>
2014-08-21 00:23:50 -0400

    123

--- example.js
+++ example.js
@@ -1,14 +1,14 @@
 var myApp = angular.module('plunker', ['ui.bootstrap']);
 
 myApp.controller('dropDownController', ['$scope', function($scope) {
-    $scope.status = {
-            isopen: false
-          };
+   $scope.status = {
+      isopen: false
+   };
 
-          $scope.toggleDropdown = function($event) {
-            $event.preventDefault();
-            $event.stopPropagation();
-            $scope.status.isopen = !$scope.status.isopen;
-          };
-  }]);
+   $scope.toggleDropdown = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+      $scope.status.isopen = !$scope.status.isopen;
+   };
+}]);
 
