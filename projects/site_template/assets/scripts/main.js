7a1d2cf1f3f74db48646b85ede5a0e54e14600af
Thierry Mayrand <thierry.mayrand@gmail.com>
2014-11-06 21:43:44 -0500

    123

--- projects/site_template/assets/scripts/main.js
+++ projects/site_template/assets/scripts/main.js
@@ -1,1 +1,14 @@
-var app = angular.module('templateSite', ['ui.bootstrap', 'ngRoute']); 
+var app = angular.module('site_template', ['ui.bootstrap']);
+
+app.controller('MainCtrl', function($scope) {
+    $scope.name = 'World';
+});
+
+app.controller('DropdownCtrl', function($scope) {
+ 
+    $scope.items = [
+        "The first choice!",
+        "And another choice for you.",
+        "but wait! A third!"
+    ];
+}); 
